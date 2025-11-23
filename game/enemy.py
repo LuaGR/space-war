53fc1cdcd13569fa196bf79552dd0ed905931d02
Juan-python <sheykengamer1034@gmail.com>
2025-11-23 00:50:09 -0500

    Limpieza de memoria cuando enemigos salen de la pantalla

--- game/enemy.py
+++ game/enemy.py
@@ -1,23 +1,23 @@
-# En game/enemy.py (o donde esté definida la clase Enemy)
+
 import random
 import pygame
 from constants import SCREEN_WIDTH
 
 class Enemy:
     def __init__(self):
-        self.x = random.randint(0, SCREEN_WIDTH - 50)  # Posición aleatoria en el ancho de la pantalla
-        self.y = random.randint(50, 200)  # Posición aleatoria en el eje Y
+        self.x = random.randint(0, SCREEN_WIDTH - 50)  
+        self.y = random.randint(50, 200)  
         self.width = 50
         self.height = 50
-        self.color = (111, 0, 111)  # Color rojo
-        self.speed = 0.5  # Velocidad de movimiento
+        self.color = (111, 0, 111)  
+        self.speed = 0.5  
 
     def update(self):
         """Actualizar la posición del enemigo"""
-        self.x += self.speed  # Mover al enemigo a la derecha
+        self.x += self.speed  
         
         if self.x > SCREEN_WIDTH:
-            self.x = 0  # Si el enemigo se sale por la derecha, aparece de nuevo en la izquierda
+            self.x = 0  
 
     def draw(self, screen):
         """Dibujar al enemigo en la pantalla"""
