31551418dcaa246bfbcd4ec0de1f4bbaffd7b2e6
Juan-python <sheykengamer1034@gmail.com>
2025-12-09 10:56:10 -0500

    Implementación de reinicio con tecla enter y barra de vida

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from game.player import Player
 from game.bullet import Bullet
 from game.enemy import Enemy
 from game.score import Score
+from game.healthbar import Health_Bar
 
 TITLE = "Space War"
 FPS = 60
@@ -21,6 +22,7 @@ last_spawn_time = pygame.time.get_ticks()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption(TITLE)
 score = Score(screen)
+health_bar = Health_Bar(screen)
 
 try:
     laser_sound = pygame.mixer.Sound("assets/sounds/lazer-gun-432285.wav")
@@ -46,6 +48,7 @@ elapsed_time_sec = 0
 
 font_big = pygame.font.Font(None, 64)
 font_med = pygame.font.Font(None, 36)
+font_small = pygame.font.Font(None, 24)
 
 
 
@@ -55,7 +58,16 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
+        if game_over:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
+                game_over = False
+                start_time_ms = pygame.time.get_ticks()
+                last_spawn_time = pygame.time.get_ticks()
+                score.reset_score()
+                health_bar.reset_health()
+                player.reset_position()
+                enemies.clear
+                bullets.clear
        
         if not game_over:
             player.handle_movement(event)
@@ -79,10 +91,12 @@ while running:
         text_game_over = font_big.render("JUEGO TERMINADO", True, (255, 0, 0))
         text_score = font_med.render(f"Puntaje: {score.score}", True, (255, 255, 255))
         text_time = font_med.render(f"Tiempo: {elapsed_time_sec} s", True, (255, 255, 255))
+        text_restart = font_small.render(f"presiona ENTER para reiniciar", True, (200, 200, 200))
 
         screen.blit(text_game_over, (SCREEN_WIDTH//2 - text_game_over.get_width()//2, SCREEN_HEIGHT//2 - 100))
         screen.blit(text_score, (SCREEN_WIDTH//2 - text_score.get_width()//2, SCREEN_HEIGHT//2 - 20))
         screen.blit(text_time, (SCREEN_WIDTH//2 - text_time.get_width()//2, SCREEN_HEIGHT//2 + 30))
+        screen.blit(text_restart, (SCREEN_WIDTH//2 - text_restart.get_width()//2, SCREEN_HEIGHT//2 + 80))
 
         pygame.display.flip()
         clock.tick(FPS)
@@ -152,14 +166,22 @@ while running:
         enemy_rect = pygame.Rect(int(enemy.x), int(enemy.y), enemy.width, enemy.height)
 
         if enemy_rect.colliderect(player_rect):
-            elapsed_time_sec = (pygame.time.get_ticks() - start_time_ms) // 1000
-            game_over = True
+            if explosion_sound:
+                explosion_sound.play()
+                health_bar.lose_health()
+                enemies.remove(enemy)
+            if not health_bar.is_alive():
+                elapsed_time_sec = (pygame.time.get_ticks() - start_time_ms) // 1000
+                game_over = True
+            else:
+                player.reset_position()
             break
   
 
 
     player.draw(screen)
     score.draw()
+    health_bar.draw()
 
     pygame.display.flip()
     clock.tick(FPS)
